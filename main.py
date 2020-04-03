72569d66c79877a0eb25df8e9423065db40aeb42
Rohit Lalwani <lalwanirohit71@gmail.com>
2020-04-03 22:19:47 +0530

    Project Upload

---
+++ main.py
@@ -0,0 +1,130 @@
+from tkinter import *
+from PIL import Image, ImageFilter
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def png_to_nparray(path):
+    im = Image.open(path).convert("L")
+    width = float(im.size[0])
+    height = float(im.size[1])
+    output = Image.new("L", (28,28), (255))
+    if width>height:
+        nheight = int(round((20.0/width*height), 0))
+        if nheight==0:
+            nheight=1
+        img = im.resize((20,nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28-nheight)/2), 0))
+        output.paste(img, (4, wtop))
+    else:
+        nwidth = int(round((20.0/height*width), 0))
+        if nwidth==0:
+            nwidth=1
+        img = im.resize((nwidth,20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28-nwidth)/2), 0))
+        output.paste(img, (wleft,4))
+    output1 = list(output.getdata())
+    linear_array = [(255-x)*1.0/255.0 for x in output1]
+    arr = np.array(linear_array)
+    arr = np.reshape(arr, (28,28))
+    return arr
+
+class Paint(object):
+
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Character Recognition")
+        self.root.geometry("485x499")
+        self.root.resizable(0,0)
+        self.root.configure(background="#d9d9d9")
+
+        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
+        self.pen_button.place(relx=0.233, rely=0.066, height=33, width=106)
+        self.pen_button.configure(pady="0")
+
+        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        self.eraser_button.place(relx=0.567, rely=0.066, height=33, width=106)
+        self.eraser_button.configure(pady="0")
+
+        self.canv = Canvas(self.root, bg='white')
+        self.canv.place(relx=0.247, rely=0.214, relheight=0.501, relwidth=0.515)
+        self.canv.configure(background="#ffffff")
+        self.canv.configure(borderwidth="2")
+        self.canv.configure(cursor="dot")
+        self.canv.configure(relief="ridge")
+
+        self.reset_button = Button(self.root, text='Reset', command=self.resetall)
+        self.reset_button.place(relx=0.247, rely=0.782, height=33, width=106)
+        self.reset_button.configure(pady="0")
+
+        self.predict_button = Button(self.root, text='Predict', command=self.predict)
+        self.predict_button.place(relx=0.577, rely=0.782, height=33, width=106)
+        self.predict_button.configure(pady='0')
+
+        self.setup()
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.color = 'black'
+        self.line_width = 10.0
+        self.eraser_on = False
+        self.active_button = self.pen_button
+        self.canv.bind('<B1-Motion>', self.draw)
+        self.canv.bind('<ButtonRelease-1>', self.release_button)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button.config(relief=RAISED)
+        some_button.config(relief=SUNKEN)
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+
+    def draw(self, event):
+        draw_color = 'white' if self.eraser_on else self.color
+        if self.old_x and self.old_y:
+            self.canv.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=draw_color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def release_button(self, event):
+        self.old_x, self.old_y = None, None
+
+    def resetall(self):
+        self.old_x, self.old_y = None, None
+        self.canv.delete("all")
+
+    def predict(self):
+        self.canv.postscript(file="temp.eps")
+        img = Image.open("temp.eps")
+        img.save("temp.png")
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = tf.keras.models.model_from_json(loaded_model_json)
+        loaded_model.load_weights("model.h5")
+        arr = png_to_nparray("temp.png")
+        print("Shape of array is:", arr.shape)
+        probability_model = tf.keras.Sequential([tf.keras.layers.Reshape((28,28)), 
+                                                loaded_model, 
+                                                tf.keras.layers.Softmax()])
+        predictions = probability_model.predict(np.array([arr]))
+        chars = "0123456789abcdefghijklmnopqrstuvwxyz"
+        labelindex = np.argmax(predictions[0])
+        plt.figure()
+        plt.imshow(arr)
+        plt.xlabel(chars[labelindex])
+        plt.colorbar()
+        plt.grid(False)
+        plt.show()
+
+if __name__ == '__main__':
+    Paint()
---
+++ train.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from emnist import list_datasets
+from emnist import extract_training_samples
+from emnist import extract_test_samples
+import matplotlib.pyplot as plt
+import numpy as np
+
+# print(list_datasets())
+
+letters_images, letters_labels = extract_training_samples('letters')
+# print(images.shape)
+# print(labels.shape)
+digits_images, digits_labels = extract_training_samples('digits')
+letters_labels=letters_labels + 9
+final_images = np.concatenate((digits_images, letters_images), axis=0)
+final_labels = np.concatenate((digits_labels, letters_labels), axis=0)
+# print(final_labels)
+# print(final_images[0]/255)
+plt.imshow(final_images[0]/255)
+plt.show()
+
+test_images, test_labels = extract_test_samples('letters')
+test_images1, test_labels1 = extract_test_samples('digits')
+test_labels = test_labels + 9
+test_images = np.concatenate((test_images1, test_images), axis=0)
+test_labels = np.concatenate((test_labels1, test_labels), axis=0)
+a = []
+for i in test_labels:
+    if i not in a:
+        a.append(i)
+# print(sorted(a))
+
+final_images = final_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Reshape((28,28,1)),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Dropout(.2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(36, activation='linear')
+])
+
+model.compile(optimizer='adam',
+             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+             metrics=['accuracy'])
+
+# print(tf.keras.backend.eval(model.optimizer.lr))
+
+model.fit(final_images, final_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+# print(test_loss, test_acc)
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
